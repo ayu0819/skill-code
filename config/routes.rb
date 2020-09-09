52cbab308960759f38b6f7093bf3dcfd0db2977d
ayu0819 <ayutateishi0819@gmail.com>
2020-09-09 22:13:55 +0900

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  post "users/:id/update" => "users#update"
+  get "users/:id/edit" => "users#edit"
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/:id" => "users#show"
+  get "member" => "users#index"
   resources :posts
   get "posts" => "posts#index"
-
   get "skill" => "skill#top"
   get "/" => "home#top"
 end
